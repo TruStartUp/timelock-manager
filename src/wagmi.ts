a932b2bad1df122d1be515159307f4b97d688e50
David Carvajal <david@tropykus.com>
2025-11-28 09:50:06 -0500

    Initial commit

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,23 +1,15 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
+  rootstock,
+  rootstockTestnet
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
   chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+    rootstock,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [rootstockTestnet] : []),
   ],
   ssr: true,
 });
